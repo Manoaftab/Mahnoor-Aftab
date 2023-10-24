b82fa579e7fd2ca864f991d63dec3f208623a430
Manoaftab <145097686+Manoaftab@users.noreply.github.com>
2023-10-24 09:22:51 -0700

    Add files via upload

---
+++ MYCOUNTER/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const CounterApp = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.counterText}>Counter: {count}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Increment" onPress={incrementCount} />
+        <Button title="Decrement" onPress={decrementCount} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  counterText: {
+    fontSize: 24,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+});
+
+export default CounterApp;
+
